d2bafad70d280578b0bae138841385534b38eab9
Sylvia hany <sylviahany695@gmail.com>
2025-11-13 21:38:00 +0200

    ADD contact-aboutus pages

---
+++ CurlyQueens/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurlyQueens.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Aboutpage");
+        }
+    }
+}
---
+++ CurlyQueens/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurlyQueens.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Contactpage");
+        }
+    }
+}
